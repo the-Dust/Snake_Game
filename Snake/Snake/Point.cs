9d3f4a23ea4b5139e0529456e6e75235aa196e86
the-Dust <dust_@inbox.ru>
2016-08-28 15:38:33 +0400

    Создан класс Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+}
