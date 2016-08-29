067bc7039c0b6ecee35b666f2ec4c92724d4a428
the-Dust <dust_@inbox.ru>
2016-08-29 21:02:54 +0400

    Добавлена работа со списками

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,6 +16,24 @@ namespace ConsoleApplication1
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(1);
+            numList.Add(2);
+            numList.Add(3);
+            int x = numList[0];
+
+            numList.RemoveAt(0);
+
+
+            foreach (int i in numList)
+            {
+                Console.Write(i);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadKey();
         }
     }
